ef8112b30a50107bbd219ca08a84128c096c5a67
Ezra Huscher <waterspring@gmail.com>
2025-03-25 22:22:36 -0600

    Feedback!

--- src/cooling_sfr/feedback.cc
+++ src/cooling_sfr/feedback.cc
@@ -21,6 +21,7 @@
 #include "../system/system.h"
 #include "../time_integration/timestep.h"
 
+
 // Feedback type bitmask flags
 #define FEEDBACK_SNII  1  // Supernova Type II
 #define FEEDBACK_AGB   2  // AGB wind feedback
@@ -72,7 +73,7 @@ Yields get_SNIa_yields(double n_snia) {
     return {0.01 * n_snia, 0.0, 0.0, 0.007 * n_snia};
 }
 
-void apply_stellar_feedback(double current_time, simparticles *Sp) {
+void apply_stellar_feedback(double current_time, struct sph_data* Sp) {
     // Reset per-timestep counters
     ThisStepEnergy_SNII = 0;
     ThisStepEnergy_SNIa = 0;
@@ -81,41 +82,40 @@ void apply_stellar_feedback(double current_time, simparticles *Sp) {
     std::memset(ThisStepMetalsInjected, 0, sizeof(ThisStepMetalsInjected));
 
     for (int i = 0; i < Sp->NumPart; i++) {
-        if (P[i].Type != 4) continue;  // Only apply feedback to star particles
+        if (Sp->P[i].Type != 4) continue;  // Only apply feedback to star particles
 
-        double age = current_time - P[i].BirthTime;
-        double m_star = P[i].Mass;
+        double age = current_time - Sp->P[i].BirthTime;
+        double m_star = Sp->P[i].Mass;
 
-        // Skip particles that have completed all feedback channels
-        if (P[i].FeedbackFlag == (FEEDBACK_SNII | FEEDBACK_AGB | FEEDBACK_SNIa))
+        if (Sp->P[i].FeedbackFlag == (FEEDBACK_SNII | FEEDBACK_AGB | FEEDBACK_SNIa))
             continue;
 
-        int *ngblist = P[i].NeighborList;
-        int num_ngb = P[i].NumNeighbors;
+        int *ngblist = Sp->P[i].NeighborList;
+        int num_ngb = Sp->P[i].NumNeighbors;
         double total_w = 0.0;
         for (int n = 0; n < num_ngb; n++) {
             int j = ngblist[n];
-            if (P[j].Type != 0) continue;
-            total_w += P[i].Kernel[n];
+            if (Sp->P[j].Type != 0) continue;
+            total_w += Sp->P[i].Kernel[n];
         }
         if (total_w <= 0.0) continue;
 
         // SNII feedback
-        if (age > SNII_DELAY_TIME && !(P[i].FeedbackFlag & FEEDBACK_SNII)) {
+        if (age > SNII_DELAY_TIME && !(Sp->P[i].FeedbackFlag & FEEDBACK_SNII)) {
             double e_sn = SNII_ENERGY_PER_MASS * m_star;
             double m_return = MASS_RETURN_SNII * m_star;
             Yields y = get_SNII_yields(m_return);
             for (int n = 0; n < num_ngb; n++) {
                 int j = ngblist[n];
-                if (P[j].Type != 0) continue;
-                double w = P[i].Kernel[n] / total_w;
-                SphP[j].Energy += e_sn * w;
-                P[j].Mass += m_return * w;
-                P[j].Vel[0] += WIND_VELOCITY * w;
-                SphP[j].Metals[0] += y.Z * w;
-                SphP[j].Metals[1] += y.C * w;
-                SphP[j].Metals[2] += y.O * w;
-                SphP[j].Metals[3] += y.Fe * w;
+                if (Sp->P[j].Type != 0) continue;
+                double w = Sp->P[i].Kernel[n] / total_w;
+                Sp->SphP[j].Energy += e_sn * w;
+                Sp->P[j].Mass += m_return * w;
+                Sp->P[j].Vel[0] += WIND_VELOCITY * w;
+                Sp->SphP[j].Metals[0] += y.Z * w;
+                Sp->SphP[j].Metals[1] += y.C * w;
+                Sp->SphP[j].Metals[2] += y.O * w;
+                Sp->SphP[j].Metals[3] += y.Fe * w;
             }
             ThisStepEnergy_SNII += e_sn;
             ThisStepMassReturned += m_return;
@@ -123,24 +123,24 @@ void apply_stellar_feedback(double current_time, simparticles *Sp) {
             ThisStepMetalsInjected[1] += y.C;
             ThisStepMetalsInjected[2] += y.O;
             ThisStepMetalsInjected[3] += y.Fe;
-            P[i].FeedbackFlag |= FEEDBACK_SNII;
+            Sp->P[i].FeedbackFlag |= FEEDBACK_SNII;
         }
 
         // AGB feedback
-        if (age > SNII_DELAY_TIME && age < AGB_END_TIME && !(P[i].FeedbackFlag & FEEDBACK_AGB)) {
+        if (age > SNII_DELAY_TIME && age < AGB_END_TIME && !(Sp->P[i].FeedbackFlag & FEEDBACK_AGB)) {
             double e_agb = AGB_ENERGY_PER_MASS * m_star;
             double m_return = MASS_RETURN_AGB * m_star;
             Yields y = get_AGB_yields(m_return);
             for (int n = 0; n < num_ngb; n++) {
                 int j = ngblist[n];
-                if (P[j].Type != 0) continue;
-                double w = P[i].Kernel[n] / total_w;
-                SphP[j].Energy += e_agb * w;
-                P[j].Mass += m_return * w;
-                SphP[j].Metals[0] += y.Z * w;
-                SphP[j].Metals[1] += y.C * w;
-                SphP[j].Metals[2] += y.O * w;
-                SphP[j].Metals[3] += y.Fe * w;
+                if (Sp->P[j].Type != 0) continue;
+                double w = Sp->P[i].Kernel[n] / total_w;
+                Sp->SphP[j].Energy += e_agb * w;
+                Sp->P[j].Mass += m_return * w;
+                Sp->SphP[j].Metals[0] += y.Z * w;
+                Sp->SphP[j].Metals[1] += y.C * w;
+                Sp->SphP[j].Metals[2] += y.O * w;
+                Sp->SphP[j].Metals[3] += y.Fe * w;
             }
             ThisStepEnergy_AGB += e_agb;
             ThisStepMassReturned += m_return;
@@ -148,38 +148,36 @@ void apply_stellar_feedback(double current_time, simparticles *Sp) {
             ThisStepMetalsInjected[1] += y.C;
             ThisStepMetalsInjected[2] += y.O;
             ThisStepMetalsInjected[3] += y.Fe;
-            P[i].FeedbackFlag |= FEEDBACK_AGB;
+            Sp->P[i].FeedbackFlag |= FEEDBACK_AGB;
         }
 
         // SNIa feedback
-        if (age > SNIa_DELAY_TIME && !(P[i].FeedbackFlag & FEEDBACK_SNIa)) {
+        if (age > SNIa_DELAY_TIME && !(Sp->P[i].FeedbackFlag & FEEDBACK_SNIa)) {
             double n_snia = m_star * SNIa_RATE_PER_MASS;
             double e_snia = n_snia * SNIa_ENERGY_PER_EVENT;
             Yields y = get_SNIa_yields(n_snia);
             for (int n = 0; n < num_ngb; n++) {
                 int j = ngblist[n];
-                if (P[j].Type != 0) continue;
-                double w = P[i].Kernel[n] / total_w;
-                SphP[j].Energy += e_snia * w;
-                SphP[j].Metals[0] += y.Z * w;
-                SphP[j].Metals[1] += y.C * w;
-                SphP[j].Metals[2] += y.O * w;
-                SphP[j].Metals[3] += y.Fe * w;
+                if (Sp->P[j].Type != 0) continue;
+                double w = Sp->P[i].Kernel[n] / total_w;
+                Sp->SphP[j].Energy += e_snia * w;
+                Sp->SphP[j].Metals[0] += y.Z * w;
+                Sp->SphP[j].Metals[1] += y.C * w;
+                Sp->SphP[j].Metals[2] += y.O * w;
+                Sp->SphP[j].Metals[3] += y.Fe * w;
             }
             ThisStepEnergy_SNIa += e_snia;
             ThisStepMetalsInjected[0] += y.Z;
             ThisStepMetalsInjected[1] += y.C;
             ThisStepMetalsInjected[2] += y.O;
             ThisStepMetalsInjected[3] += y.Fe;
-            P[i].FeedbackFlag |= FEEDBACK_SNIa;
+            Sp->P[i].FeedbackFlag |= FEEDBACK_SNIa;
         }
 
-        // If all types of feedback are complete, set final flag
-        if ((P[i].FeedbackFlag & (FEEDBACK_SNII | FEEDBACK_AGB | FEEDBACK_SNIa)) == (FEEDBACK_SNII | FEEDBACK_AGB | FEEDBACK_SNIa))
-            P[i].FeedbackFlag = FEEDBACK_SNII | FEEDBACK_AGB | FEEDBACK_SNIa;
+        if ((Sp->P[i].FeedbackFlag & (FEEDBACK_SNII | FEEDBACK_AGB | FEEDBACK_SNIa)) == (FEEDBACK_SNII | FEEDBACK_AGB | FEEDBACK_SNIa))
+            Sp->P[i].FeedbackFlag = FEEDBACK_SNII | FEEDBACK_AGB | FEEDBACK_SNIa;
     }
 
-    // Accumulate timestep values into cumulative totals
     TotalEnergyInjected_SNII += ThisStepEnergy_SNII;
     TotalEnergyInjected_SNIa += ThisStepEnergy_SNIa;
     TotalEnergyInjected_AGB  += ThisStepEnergy_AGB;
@@ -187,7 +185,6 @@ void apply_stellar_feedback(double current_time, simparticles *Sp) {
     for (int k = 0; k < 4; k++)
         TotalMetalsInjected[k] += ThisStepMetalsInjected[k];
 
-    // Output per-timestep summary
     if(ThisTask == 0) {
         printf("[Feedback Timestep Summary] E_SNII=%.3e erg, E_SNIa=%.3e erg, E_AGB=%.3e erg\n",
                ThisStepEnergy_SNII, ThisStepEnergy_SNIa, ThisStepEnergy_AGB);
@@ -196,5 +193,6 @@ void apply_stellar_feedback(double current_time, simparticles *Sp) {
                ThisStepMetalsInjected[0], ThisStepMetalsInjected[1], ThisStepMetalsInjected[2], ThisStepMetalsInjected[3]);
     }
 }
+
 
 #endif // FEEDBACK
