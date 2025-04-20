dac6d954ccc8a895c2115639eeb775fc2af528ef
Ezra Huscher <waterspring@gmail.com>
2025-04-20 15:04:59 -0600

    asdf

--- src/cooling_sfr/feedback.cc
+++ src/cooling_sfr/feedback.cc
@@ -554,11 +554,12 @@ void feedback_ngb(FeedbackInput *in, FeedbackResult *out, int j, FeedbackWalk *f
 
 
     //double delta_u = E_therm_j * erg_to_code / gas_mass;
-    double delta_u = E_therm_j * erg_per_mass_to_code / gas_mass;
+    double gas_mass_cgs = gas_mass * All.UnitMass_in_g;
+    double delta_u = E_therm_j * erg_per_mass_to_code / gas_mass_cgs;
 
-    printf("[Feedback DEBUG] GasID=%d | E_therm_j=%.3e erg | gas_mass=%.3e g | erg_per_mass_to_code=%.3e | delta_u=%.3e (u_before=%.3e)\n",
+    printf("[Feedback DEBUG] GasID=%d | E_therm_j=%.3e erg | gas_mass_cgs=%.3e g | erg_per_mass_to_code=%.3e | delta_u=%.3e (u_before=%.3e)\n",
         Sp->P[j].ID.get(),
-        E_therm_j, gas_mass, erg_per_mass_to_code, delta_u, Sp->get_utherm_from_entropy(j));
+        E_therm_j, gas_mass_cgs, erg_per_mass_to_code, delta_u, Sp->get_utherm_from_entropy(j));
 
     if (!isfinite(delta_u) || delta_u < 0) {
         FEEDBACK_PRINT("[Feedback WARNING] Non-finite delta_u = %.3e for gas %d\n", delta_u, j);
