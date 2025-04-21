d1befdec5c30e48bc628ef76ec718b8f656859d3
Ezra Huscher <waterspring@gmail.com>
2025-04-20 23:18:54 -0600

    asdf

--- src/cooling_sfr/feedback.h
+++ src/cooling_sfr/feedback.h
@@ -79,6 +79,9 @@ double intpos_to_kpc(MyIntPosType ipos);
 void feedback_tree(int *active_list, int num_active);
 int feedback_tree_evaluate(int target, int mode, int threadid);
 
+double adaptive_feedback_radius(MyDouble Pos[3], int feedback_type, simparticles *Sp,
+    int *neighbors_ptr, void *unused1, int unused2);
+
 // ------------------------
 // Function Declarations
 // ------------------------
