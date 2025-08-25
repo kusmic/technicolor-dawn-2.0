ec8e5d68e6bf2e57991e9e500d1e8f0839809d85
kusmic <samirkusmic@gmail.com>
2025-08-25 09:57:42 -0500

    Added USE_CUDA config option

--- Template-Config.sh
+++ Template-Config.sh
@@ -226,3 +226,6 @@ INITIAL_CONDITIONS_CONTAIN_ENTROPY
 #STOP_AFTER_STEP=10                           # ends a simulation after the specified timestep (to simplify scalability tests)
 #TREE_NO_SAFETY_BOX                           # when set, this disables the geometric 'near node' protection
 
+#---------------------------------------- Miscellaneous options
+
+#USE_CUDA                                     # enables GPU support through CUDA    
