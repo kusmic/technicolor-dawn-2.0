7ffcb347a12bdfcb80a622843038f13cb617484f
kusmic <samirkusmic@gmail.com>
2025-09-11 15:10:54 -0500

    CUDA refactored fully gwalk_cuda.cu and cuh

--- src/gravtree/gwalk_cuda.cuh
+++ src/gravtree/gwalk_cuda.cuh
@@ -15,12 +15,19 @@
 #include "gadgetconfig.h"
 
 #include "../mpi_utils/shared_mem_handler.h"
-#include <cuda_runtime.h
+#include <cuda_runtime.h>
+
+// Forward declarations
+struct DeviceData;
+extern __device__ DeviceData d_data;
 
 class gwalk : public gravtree<simparticles>
 {
  public:
   void gravity_tree(int timebin);
+  void initialize_cuda_data(const pinfo* h_pdats, const gravnode* h_nodes, 
+                          const particle_data* h_particles, int n);
+  void cleanup_cuda_data();
 
  private:
   long long interactioncountPP;
@@ -111,12 +118,23 @@ class gwalk : public gravtree<simparticles>
     return ptype;
   }
 
-  inline void gwalk_open_node(const pinfo &pdat, int i, char ptype, gravnode *nop, int mintopleafnode, int committed);
-  void gravity_force_interact(const pinfo &pdat, int i, int no, char ptype, char no_type, unsigned char shmrank, int mintopleafnode,
-                              int committed);
+  __device__ void gwalk_open_node(const pinfo &pdat, int i, char ptype, 
+                                 gravnode *nop, int mintopleafnode, int committed);
 
-  inline int evaluate_particle_node_opening_criterion_and_interaction(const pinfo &pdat, gravnode *nop);
-  __host__ __device__ inline void evaluate_particle_particle_interaction(const pinfo &pdat, const int no, const char jtype, int no_task);
+  __device__ void gravity_force_interact(const pinfo &pdat, int i, int no, 
+                                       char ptype, char no_type, unsigned char shmrank,
+                                       int mintopleafnode, int committed);
+
+  __device__ int evaluate_particle_node_opening_criterion_and_interaction(
+      const pinfo &pdat, gravnode *nop);
+
+  __device__ void evaluate_particle_particle_interaction(
+      const pinfo &pdat, const int no, const char jtype, int shmrank);
 };
+
+// CUDA kernel declaration
+__global__ void gravity_force_interact_kernel(const pinfo *pdats, int *is, 
+    int *nos, char *ptypes, char *no_types, unsigned char *shmranks, 
+    int *mintopleafnodes, int *committeds, int n);
 
 #endif
